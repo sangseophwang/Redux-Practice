ad60abb7551c8c2452699d3a88592d2c4b1df4ca
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-06 19:13:59 +0900

    feat: redux로 비동기 처리 학습

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -26,3 +26,33 @@ export function showAll() {
 export function showComplete() {
   return { type: SHOW_COMPLETE };
 }
+
+// users
+
+// Github API 호출을 시작하는 것을 의미
+export const GET_USERS_START = "GET_USERS_START";
+
+// Github API 호출에 대한 응답이 성공적으로 돌아온 경우
+export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS";
+
+// Github API 호출에 대한 응답이 실패한 경우
+export const GET_USERS_FAIL = "GET_USERS_FAIL";
+
+export function getUsersStart() {
+  return {
+    type: GET_USERS_START,
+  };
+}
+
+export function getUsersSuccess(data) {
+  return {
+    type: GET_USERS_SUCCESS,
+    data,
+  };
+}
+export function getUsersFail(error) {
+  return {
+    type: GET_USERS_FAIL,
+    error,
+  };
+}
