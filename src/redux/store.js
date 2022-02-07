4e64ab6b2647f49d62c0638f0e921175d6f73bc6
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-07 09:02:47 +0900

    feat: 리덕스 미들웨어 학습

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,19 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import todoApp from "./reducers";
 
-const store = createStore(todoApp);
+function middleware1(store) {
+  console.log("middleware1", 0);
+  return (next) => {
+    console.log("middleware1", 1);
+    return (action) => {
+      console.log("middleware1", 2);
+      const returnValue = next(action);
+      console.log("middleware1", 3);
+      return returnValue;
+    };
+  };
+}
+
+const store = createStore(todoApp, applyMiddleware(middleware1));
 
 export default store;
