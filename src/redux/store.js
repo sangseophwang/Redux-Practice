ba27580b7f31fe146d775f6730d35b096d9f6a95
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-07 09:08:27 +0900

    feat: redux devtools extension 설치

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import todoApp from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-function middleware1(store) {
-  console.log("middleware1", 0);
-  return (next) => {
-    console.log("middleware1", 1);
-    return (action) => {
-      console.log("middleware1", 2);
-      const returnValue = next(action);
-      console.log("middleware1", 3);
-      return returnValue;
-    };
-  };
-}
-
-const store = createStore(todoApp, applyMiddleware(middleware1));
+const store = createStore(todoApp, composeWithDevTools(applyMiddleware()));
 
 export default store;
