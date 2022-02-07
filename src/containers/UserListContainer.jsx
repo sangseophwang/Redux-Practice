c70b3dfb7d1b0e83c00c9d98447a1b077a44097d
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-07 09:31:05 +0900

    feat: redux-thunk 학습

--- src/containers/UserListContainer.jsx
+++ src/containers/UserListContainer.jsx
@@ -2,21 +2,14 @@ import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import UserList from "../components/UserList";
-import { getUsersFail, getUsersStart, getUsersSuccess } from "../redux/actions";
-import axios from "axios";
+import { getUsersThunk } from "../redux/actions";
 
 export default function UserListContainer() {
   const users = useSelector((state) => state.users.data);
   const dispatch = useDispatch();
 
-  const getUsers = useCallback(async () => {
-    try {
-      dispatch(getUsersStart());
-      const res = await axios.get("https://api.github.com/users");
-      dispatch(getUsersSuccess(res.data));
-    } catch (error) {
-      dispatch(getUsersFail(error));
-    }
+  const getUsers = useCallback(() => {
+    dispatch(getUsersThunk());
   }, [dispatch]);
 
   return <UserList users={users} getUsers={getUsers} />;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import todoApp from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-const store = createStore(todoApp, composeWithDevTools(applyMiddleware()));
+const store = createStore(todoApp, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
